a427fde7268fbf543739528e2d569ee09c75d995
GregDamsky <gregdamsky@gmail.com>
2024-02-18 11:08:03 +0200

    deleting ronaldo

--- src/_codux/boards/ronaldo/ronaldo.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { Ronaldo } from '../../../components/ronaldo/ronaldo';
-
-export default createBoard({
-    name: 'Ronaldo',
-    Board: () => <Ronaldo />,
-    isSnippet: true,
-});
