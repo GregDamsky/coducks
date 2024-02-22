df1abacb3baba76ec758af342dddb41fa5e037ac
GregDamsky <143331639+GregDamsky@users.noreply.github.com>
2024-02-22 16:47:38 +0200

    Greg/changes2 (#2)
    * adding a new component
    
    * editing a new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,44 @@
-import { Header } from "./components/header";
-import { Images } from "./components/images";
-import { ProductInfo } from "./components/product-info";
-import { useCart } from "./hooks";
-import type { Product, User } from "./types";
+import './App.css';
+import { Header } from './components/header';
+import { Images } from './components/images';
+import { ProductInfo } from './components/product-info';
+import { useCart } from './hooks';
+import type { Product, User } from './types';
 
 function App({ product, user }: { product: Product; user: User }) {
-  const [cart, addToCart] = useCart();
+    const [cart, addToCart] = useCart();
 
-  return (
-    <div>
-      <Header cart={cart} user={user} />
+    return (
+        <div>
+            <Header cart={cart} user={user} />
 
-      <main className="grid grid-cols-1 md:grid-cols-2 grid-rows-auto gap-2">
-        <Images images={product.images} />
-        <ProductInfo product={product} addToCart={addToCart} />
-      </main>
+            <main className="grid grid-cols-1 md:grid-cols-2 grid-rows-auto gap-2 gggg tttt yyyyyy eeeee qqqqq mmmm eeeww3 zzzz xxxxx bg-slate-500 m-11 z-50 bg-blend-color-dodge colo">
+                <Images images={product.images} />
+                <ProductInfo product={product} addToCart={addToCart} />
+                <div>
+                    <div>
+                        <span></span>
+                    </div>
+                </div>
+            </main>
 
-      <footer className="text-center text-sm p-5">
-        Made with ♥ in&nbsp;
-        <a href="https://codux.com">Codux</a>
-        &nbsp;with&nbsp;
-        <a href="https://radix-ui.com">RadixUI</a>
-        &nbsp;and&nbsp;
-        <a href="https://tailwindcss.com">TailwindCSS</a>
-      </footer>
-    </div>
-  );
+            <footer className="text-center text-sm p-5" onClick={()=>{console.log("hello")}}>
+                Made with ♥ in&nbsp;
+                <a
+                    href="https://codux.com"
+                    onClick={() => {
+                        console.log('Hello');
+                    }}
+                >
+                    Codux
+                </a>
+                &nbsp;with&nbsp;
+                <a href="https://radix-ui.com">RadixUI</a>
+                &nbsp;and&nbsp;
+                <a href="https://tailwindcss.com">TailwindCSS</a>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/new-component-7777/new-component-7777.tsx
@@ -0,0 +1,25 @@
+import './new-component-7777.css';
+export interface NewComponent7777Props {
+    className?: string;
+    children?: React.ReactNode;
+}
+
+/**
+ * This component was generated using Codux's built-in Default new component template.
+ * For details on how to create custom new component templates, see https://help.codux.com/kb/en/article/kb16522
+ */
+export const NewComponent7777 = ({
+    children = 'NewComponent7777',
+}: NewComponent7777Props) => {
+    return (
+        <div className="text-center title">
+            <ul className="unordered">
+                <li className="list">{children}Item3222</li>
+                <li className="li-1 li-2 li-4 li-5 li-3">Item1444</li>
+                <li>Coffee</li>
+                <li>Tea</li>
+                <li>Milk</li>
+            </ul>
+        </div>
+    );
+};
